a8f8d375727a127a1e42c109c82f5ab155d0cf38
Brian E. Granger <ellisonbg@gmail.com>
2017-05-24 15:49:21 -0700

    Build working!

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-__webpack_public_path__ = PageConfig.getOption('publicUrl');
+
 
 require('es6-promise/auto');  // polyfill Promise on IE'
-require('font-awesome/css/font-awesome.min.css');
-require('@jupyterlab/default-theme/style/index.css');
 
 var app = require('@jupyterlab/application').JupyterLab;
 var PageConfig = require('@jupyterlab/coreutils').PageConfig;
+__webpack_public_path__ = PageConfig.getOption('publicUrl');
 
+require('font-awesome/css/font-awesome.min.css');
+require('@jupyterlab/default-theme/style/index.css');
 
 function main() {
     var version = PageConfig.getOption('appVersion') || 'unknown';
