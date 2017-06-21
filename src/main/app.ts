9f900c349791f281a098bdd260690d89734b9d50
Luc Bouchard <lucf@bouchard.com>
2017-06-21 16:55:34 -0700

    Start jupyter server and launch render process with appropriate token

--- src/main/app.ts
+++ src/main/app.ts
@@ -1,58 +1,127 @@
+// Copyright (c) Jupyter Development Team.
+// Distributed under the terms of the Modified BSD License.
 
-import { dialog, app, ipcMain, BrowserWindow } from 'electron'
+import { dialog, app, BrowserWindow } from 'electron'
 import { ChildProcess, spawn } from 'child_process';
+import * as Handlebars from 'handlebars';
+import * as fs from 'fs';
 import * as path from 'path';
-import * as url from 'url';
+import * as url from 'url'
 
 class JupyterServer {
+    /**
+     * The child process object for the Jupyter server
+     */
     private nbServer: ChildProcess;
+
+    /**
+     * The Jupyter server authentication token
+     */
     public token: string;
+
+    /**
+     * The Jupyter server hostname
+     */
     public hostname: string;
-    public port: string;
+
+    /**
+     * The Jupyter server port number
+     */
+    public port: number;
+
+    /**
+     * The Jupyter server url
+     */
     public url: string;
+
+    /**
+     * The jupyter server notebook directory
+     */
     public notebookDir: string;
     
-    public start(callback: () => void): void {
-        let listRun = false;
-        this.nbServer = spawn('jupyter', ['notebook', '--no-browser', '--debug']);
-        this.nbServer.stderr.on('data', (serverData: string) => {
-            //if (serverData.search("The Jupyter Notebook is running at") == -1)
-            //    return;
-            console.log(String(serverData));
-            if (listRun) {
-                return;
-            }
+    /**
+     * Start a local Jupyer server on the specified port. Returns
+     * a promise that is fulfilled when the Jupyter server has
+     * started and all the required data (url, token, etc.) is
+     * collected. This data is collected using `jupyter notebook list`
+     */
+    public start(port: number): Promise<any> {
+        return new Promise((resolve, reject) => {
+            this.nbServer = spawn('jupyter', ['notebook', '--no-browser', '--port', String(port)]);
 
-            listRun = true;
-            // Get server data
-            let list = spawn('jupyter', ['notebook', 'list', '--json']);
-            list.stdout.on('data', (data: string) => {
-                console.log(String(data));
-                let serverData = JSON.parse(data);
-                this.token = serverData.token;
-                this.hostname = serverData.hostname;
-                this.port = serverData.port;
-                this.url = serverData.url;
-                this.notebookDir = serverData.notebook_dir;
-                callback();
+            this.nbServer.on('error', (err: Error) => {
+                this.nbServer.stderr.removeAllListeners();
+                reject(err);
+            });
+
+            this.nbServer.stderr.on('data', (serverData: string) => {
+                if (serverData.indexOf("The Jupyter Notebook is running at") == -1)
+                    return;
+                this.nbServer.removeAllListeners();
+                this.nbServer.stderr.removeAllListeners();
+                
+                /* Get server data */
+                let list = spawn('jupyter', ['notebook', 'list', '--json']);
+
+                list.on('error', (err: Error) => {
+                    list.stdout.removeAllListeners();
+                    reject(err);
+                });
+
+                list.stdout.on('data', (data: string) => {
+                    let serverData = JSON.parse(data);
+                    serverData.port = Number(serverData.port);
+                    this.token = serverData.token;
+                    this.hostname = serverData.hostname;
+                    this.port = serverData.port;
+                    this.url = serverData.url;
+                    this.notebookDir = serverData.notebook_dir;
+                    resolve(serverData);
+                });
             });
         });
     }
 
+    /**
+     * Stop the currently executing Jupyter server
+     */
     public stop(): void {
-        this.nbServer.kill();
+        if (this.nbServer !== undefined)
+            this.nbServer.kill();
     }
 }
 
 export class JupyterApplication {
+    /**
+     * The JupyterServer the application will use
+     */
     private server: JupyterServer;
+
+    /**
+     * The JupyterLab window
+     */
     private mainWindow: any;
 
+    /**
+     * The file that stores index.html after it has been
+     * run through the templater. This is a hack that should
+     * be replaced when the JupyterLab application api is 
+     * updated.
+     */
+    private indexFile: string;
+
+    /**
+     * Construct the Jupyter application
+     */
     constructor() {
         this.registerListeners();
         this.server = new JupyterServer();
+        this.indexFile = path.join(__dirname, 'temp.index.html');
     }
 
+    /**
+     * Register all application event listeners
+     */
     private registerListeners(): void {
         // On OS X it is common for applications and their menu bar to stay 
         // active until the user quits explicitly with Cmd + Q.
@@ -73,11 +142,15 @@ export class JupyterApplication {
 
         app.on('quit', () => {
             this.server.stop();
+            fs.unlinkSync(this.indexFile);
         });
     }
 
+    /**
+     * Creates the primary application window and loads the
+     * html
+     */
     private createWindow(): void {
-        // Consider creating a custom window class
         this.mainWindow = new BrowserWindow({
             width: 800,
             height: 600,
@@ -86,7 +159,7 @@ export class JupyterApplication {
         });
 
         this.mainWindow.loadURL(url.format({
-            pathname: path.join(__dirname, '../browser/index.html'),
+            pathname: this.indexFile,
             protocol: 'file:',
             slashes: true
         }));
@@ -111,19 +184,26 @@ export class JupyterApplication {
         });
     }
 
+    /**
+     * Starts the Jupyter Server and launches the electron application.
+     * When the Jupyter Sevrer start promise is fulfilled, the Handlebars
+     * templater is run on index.html and the output is saved to a file.
+     * The electron render process is then started on that file by
+     * calling createWindow
+     */
     public start(): void {
-        this.server.start(() => {
-            // Jupyter server started, start render process
-            ipcMain.on('server-data', (evt: any, arg: any) => {
-                let serverData = {
-                    token: this.server.token,
-                    url: this.server.url
-                };
-                evt.sender.send('server-data', JSON.stringify(serverData));
+        this.server.start(8888)
+            .then((serverData) => {
+                console.log("Jupyter Server started at: " + serverData.url + "?token=" + serverData.token);
+                let source = fs.readFileSync(path.join(__dirname, '../browser/index.html')).toString();
+                let template = Handlebars.compile(source);
+                let html = template(serverData);
+                fs.writeFileSync(path.resolve(__dirname, this.indexFile), html);
+                this.createWindow();
+            })
+            .catch((err) => {
+                console.error("Failed to start Jupyter Server");
+                console.error(err);
             });
-
-            this.createWindow();
-        });
     }
-
 }
