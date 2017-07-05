9c79d92ac069d26a254d122c35e0cf2192a5f810
Luc Bouchard <lucf@bouchard.com>
2017-07-05 10:17:10 -0700

    Add debug tools

--- src/main/main.ts
+++ src/main/main.ts
@@ -1,6 +1,12 @@
 import {app} from 'electron'
 import {JupyterApplication} from './app';
 
+/**
+ * Require debugging tools. Only
+ * runs when in development.
+ */
+require('electron-debug')({showDevTools: false});
+
 app.on('ready', () => {
   let jupyterApp = new JupyterApplication();
   jupyterApp.start();
