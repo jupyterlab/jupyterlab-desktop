dcd0bca660b58e70de477bb0a4262869a3e9f4c9
Luc Bouchard <lucf@bouchard.com>
2017-07-13 11:05:02 -0700

    Changed docs to reflect new API

--- src/main/state.ts
+++ src/main/state.ts
@@ -22,35 +22,51 @@ export
 class ElectronStateDB implements IStateDB {
 
     /**
-     * The path to the platform-specific user data directory.
+     * The path to the platform-specific application data directory.
      */
     readonly path: string = app.getPath('userData');
 
     /**
-     * The name specific to the StateDb instance.
+     * The namespace prefix for all state database entries.
+     *
+     * #### Notes
+     * This value should be set at instantiation and will only be used internally
+     * by a state database. That means, for example, that an app could have
+     * multiple, mutually exclusive state databases.
      */
     readonly namespace: string;
 
     /**
-     * Unused field in ElectronStateDB
+     * Field required by IStateDB. Not used in this
+     * implementation.
      */
     readonly maxLength: number;
 
     /**
-     * The path to the data file.
+     * The full path to the state data file.
      */
     private dataFile: string;
 
     private cache: JSONObject = null;
 
     /**
-     * Promise fulfilled when a write finishes
+     * Promise fulfilled when a write finishes. Used with
+     * the writeInProgress boolean to prevent reading/writing
+     * race conditions.
      */
     private written: Promise<void>;
 
+    /**
+     * Boolean storing write status of stateDB. Used
+     * to prevent writing/reading race conditions.
+     */
     private writeInProgress: boolean = false;
 
-
+    /**
+     * Create a new state database.
+     * 
+     * @param options - The istantiation options for the database
+     */
     constructor(options: ElectronStateDB.IOptions) {
         this.namespace = options.namespace;
         this.dataFile = this.path + '/' + this.namespace;
@@ -60,7 +76,7 @@ class ElectronStateDB implements IStateDB {
      * Check if there is a write currently in progress. If there is,
      * wait until the write promise is fulfilled.
      * 
-     * @param cb callback called when writing completes.
+     * @param cb - callback called when writing completes.
      */
     private checkWriteInProgress(cb: () => void): void {
         if (this.writeInProgress) {
@@ -77,7 +93,13 @@ class ElectronStateDB implements IStateDB {
         cb();
     }
     
-
+    /**
+     * Internal save function. Does not check the
+     * current write state. That should be implemented by
+     * the calling function.
+     * 
+     * @return A promise that is fulfilled when data is written.
+     */
     private _save(): Promise<void> {
         return new Promise<void>((res, rej) => {
             /* Signal write in progress */
@@ -96,26 +118,15 @@ class ElectronStateDB implements IStateDB {
     }
 
     /**
-     * Convert a javascript object to JSON, and write to a file. 
-     * Creates promise that is fulfilled when writing is complete.
-     * Waits until all currently executing writes are complete.
+     * Update the internal copy of the database. Waits
+     * for any writes to complete before updating the cache.
+     * The database file is only read if the cache is null since
+     * the cache will always be up-to-date otherwise. This is
+     * because all calls to 'write' and 'remove' update the
+     * internal cache.
      * 
-     * @param data javascript object to write.
-     * @param err_cb callback called in case of error.
+     * @return A promise fulfilled when the cache is updated.
      */
-    save(id: string, value: JSONObject): Promise<void> {
-        return new Promise<void>((res, rej) => {
-            this.updateCache()
-                .then(() => {
-                    this.cache[id] = value;
-                    res(this._save());
-                })
-                .catch(() => {
-                    rej();
-                })
-        });
-    }
-
     private updateCache(): Promise<void> {
         return new Promise<void>((res, rej) => {
             this.checkWriteInProgress(() => {
@@ -151,15 +162,51 @@ class ElectronStateDB implements IStateDB {
     }
 
     /**
-     * Read json data from a file and parse into
-     * the `state` attribute. If the file does not
-     * exist, the `state` attribute remains as is.
-     * 
-     * @param cb callback called when data is avaiable.
-     * 
-     * @return a promise that is fullfilled when the data is available or when
-     *         the file is found to not exist. If reading fails, the promise
-     *         is rejected.
+     * Save a value in the database.
+     *
+     * @param id - The identifier for the data being saved.
+     *
+     * @param value - The data being saved.
+     *
+     * @returns A promise that is rejected if saving fails and succeeds otherwise.
+     *
+     * #### Notes
+     * The `id` values of stored items in the state database are formatted:
+     * `'namespace:identifier'`, which is the same convention that command
+     * identifiers in JupyterLab use as well. While this is not a technical
+     * requirement for `fetch()`, `remove()`, and `save()`, it *is* necessary for
+     * using the `fetchNamespace()` method.
+     */
+    save(id: string, value: JSONObject): Promise<void> {
+        return new Promise<void>((res, rej) => {
+            this.updateCache()
+                .then(() => {
+                    this.cache[id] = value;
+                    res(this._save());
+                })
+                .catch(() => {
+                    rej();
+                })
+        });
+    }
+
+
+    /**
+     * Retrieve a saved bundle from the database.
+     *
+     * @param id - The identifier used to retrieve a data bundle.
+     *
+     * @returns A promise that bears a data payload if available.
+     *
+     * #### Notes
+     * The `id` values of stored items in the state database are formatted:
+     * `'namespace:identifier'`, which is the same convention that command
+     * identifiers in JupyterLab use as well. While this is not a technical
+     * requirement for `fetch()`, `remove()`, and `save()`, it *is* necessary for
+     * using the `fetchNamespace()` method.
+     *
+     * The promise returned by this method may be rejected if an error occurs in
+     * retrieving the data. Non-existence of an `id` will succeed with `null`.
      */
     fetch(id: string): Promise<JSONObject | null> {
         return new Promise<JSONObject | null>((res, rej) => {
@@ -178,6 +225,22 @@ class ElectronStateDB implements IStateDB {
         });
     }
 
+    /**
+     * Retrieve all the saved bundles for a namespace.
+     *
+     * @param namespace - The namespace to retrieve.
+     *
+     * @returns A promise that bears a collection data payloads for a namespace.
+     *
+     * #### Notes
+     * Namespaces are entirely conventional entities. The `id` values of stored
+     * items in the state database are formatted: `'namespace:identifier'`, which
+     * is the same convention that command identifiers in JupyterLab use as well.
+     *
+     * If there are any errors in retrieving the data, they will be logged to the
+     * console in order to optimistically return any extant data without failing.
+     * This promise will always succeed.
+     */
     fetchNamespace(namespace: string): Promise<IStateItem[]> {
         return new Promise<IStateItem[]>((res, rej) => {
             const prefix = `${this.namespace}:${namespace}:`;
@@ -206,7 +269,14 @@ class ElectronStateDB implements IStateDB {
             
         });
     }
-
+    
+    /**
+     * Remove a value from the database.
+     *
+     * @param id - The identifier for the data being removed.
+     *
+     * @returns A promise that is rejected if remove fails and succeeds otherwise.
+     */
     remove(id: string): Promise<void> {
         return new Promise<void>((res, rej) => {
             this.updateCache()
