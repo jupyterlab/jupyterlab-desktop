bc849eddf6ba222262cdf0acfcbf40a886af0c96
Natasha Proctor <nproctor@calpoly.edu>
2017-07-13 15:40:11 -0700

    Remove listeners when done

--- src/browser/index.ts
+++ src/browser/index.ts
@@ -24,19 +24,18 @@ function main() : void {
     let devMode : string  = PageConfig.getOption('devMode') || 'false';
     let settingsDir : string = PageConfig.getOption('settingsDir') || '';
     let assetsDir : string = PageConfig.getOption('assetsDir') || '';
+
+    let moon = document.getElementById('moon1');
+    let splash = document.getElementById('universe');
     
     if (version[0] === 'v') {
         version = version.slice(1);
     }
 
-    // Promise for animation listener
-    let promise : Promise<string>;
-    document.getElementById('moon1').addEventListener('animationiteration', () => {
-        promise.then( (animation) => {
-            document.getElementById('universe').style.animation = animation;
-        })
+    splash.addEventListener('animationend', () => {
+        splash.style.visibility = 'hidden';
+        splash.removeEventListener('animationend', null);
     });
-
 
     let lab = new app({
         namespace: namespace,
@@ -64,23 +63,21 @@ function main() : void {
     }
 
     // Get token from server
-    ipcRenderer.on(Channels.SERVER_DATA, (event: any, data: any) => {
+    ipcRenderer.on(Channels.SERVER_DATA, function startlab(event: any, data: any) {
         // Set token
         PageConfig.setOption("token", data.token);
         // Set baseUrl
         PageConfig.setOption("baseUrl", data.baseUrl);
         // Start lab and fade splash
-        promise = new Promise((resolve, reject) => {
-            try{
-                lab.start({ "ignorePlugins": ignorePlugins });
-                resolve("fade .4s linear 0s forwards");
-            }
-            catch (e){
-                reject(e);
-            }
+        lab.start({ "ignorePlugins": ignorePlugins })
+        .then( () => {
+            moon.addEventListener('animationiteration', function fade() {
+                splash.style.animation = "fade .4s linear 0s forwards";
+                moon.removeEventListener('animationiteration', fade);
+            });
         });
+        ipcRenderer.removeListener(Channels.SERVER_DATA, startlab);
     });
     ipcRenderer.send(Channels.RENDER_PROCESS_READY);
 }
-
 window.onload = main;
