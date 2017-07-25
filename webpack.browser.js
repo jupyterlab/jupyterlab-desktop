8d222240b5ec4fcd6cdcf812624238e8fa0cd261
Natasha R Proctor <nproctor@calpoly.edu>
2017-07-25 10:20:03 -0700

    Fix webpack html loader

--- webpack.browser.js
+++ webpack.browser.js
@@ -33,7 +33,7 @@ module.exports = {
       { test: /\.json$/, use: 'json-loader' },
       { test: /\.ts$/, use: 'awesome-typescript-loader?configFileName=./tsconfig.json' },
       { test: /\.tsx$/, use: 'awesome-typescript-loader?configFileName=./tsconfig.json' },
-      { test: /\.html$/, use: 'html-loader' },
+      { test: /\.html$/, use: 'file-loader' },
       { test: /\.(jpg|png|gif)$/, use: 'file-loader' },
       { test: /\.js.map$/, use: 'file-loader' },
       { test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/, use: 'url-loader?limit=10000&mimetype=application/font-woff' },
