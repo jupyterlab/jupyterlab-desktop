ade9d778f82a3eca60ca946077a2817769c41ec3
cameronoelsen <cameronoelsen@gmail.com>
2017-08-08 15:21:23 -0700

    Restyled the server error screen, added a new server icon

--- src/browser/components/error/servererror.tsx
+++ src/browser/components/error/servererror.tsx
@@ -3,6 +3,8 @@
 
 import * as React from 'react';
 
+let shell: Electron.Shell = (window as any).require('electron').remote.shell;
+
 export
 namespace ServerError {
     export
@@ -15,10 +17,16 @@ export
 function ServerError(props: ServerError.Props) {
     return (
         <div className='jpe-ServerError-body'>
-            <div className='jpe-ServerError-content' >
-                <h1>Something Went Wrong!</h1>
-                <p>Looks like the Jupyter Server isn't installed. Take a look at jupyter.org for help with installation.</p>
-                <button onClick={props.launchFromPath}>Choose a Path</button>
+            <div className='jpe-ServerError-content'>
+                <div className='jpe-ServerError-icon'></div>
+                <h1 className='jpe-ServerError-header'>Jupyter Server Not Found</h1>
+                <p className='jpe-ServerError-subhead'>We were unable to launch a Jupyter server, which is a prerequisite for JupyterLab Native. If you already have a Jupyter server installed, specify the path location below. Otherwise, try installing or updating Jupyter.</p>
+                <div className='jpe-ServerError-btn-container'>
+                    <button className='jpe-ServerError-btn' onClick={props.launchFromPath}>CHOOSE PATH</button>
+                    <button className='jpe-ServerError-btn' onClick={() => {
+                        shell.openExternal('https://www.jupyter.org/install.html');
+                        }}>INSTALL JUPYTER</button>
+                </div>
             </div>
         </div>
     );
