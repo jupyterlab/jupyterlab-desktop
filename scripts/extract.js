478194c35ffe31b35eab48a1e802b2edfced4991
Wietse de Vries <wietse@compenda.nl>
2018-02-07 14:34:55 +0100

    Make themes work the same way as regular JupyterLab

--- scripts/extract.js
+++ scripts/extract.js
@@ -2,6 +2,8 @@ var rpt = require('read-package-tree');
 var data = require('../package.json');
 var path = require('path');
 var fs = require('fs-extra');
+var glob = require('glob')
+var path = require('path')
 
 var seen = {};
 
@@ -42,11 +44,30 @@ function extractNode(data) {
   // Handle themes.
   var themeDir = jlab['themeDir'];
   if (themeDir) {
-    var name = data.package.name.replace('@', '');
-    name = name.replace('/', '-');
+    var themeName = data.package.name;
     var from = path.join(data.realpath, themeDir);
-    var to = path.join(themesDest, name);
+    var to = path.join(themesDest, themeName);
     fs.copySync(from, to);
+
+    /**
+     * Change relative paths. This is done at runtime by jupyterlab_launcher.
+     * A similar solution needs to be used when themes are dynamically loaded.
+     * 
+     * See https://github.com/jupyterlab/jupyterlab_launcher/blob/v0.10.3/jupyterlab_launcher/themes_handler.py
+     */
+    glob.sync(path.join(to, '**/*.css')).forEach(file => {
+      fs.readFile(file, 'utf8', function (err, data) {
+        if (err) {
+          return console.log(err);
+        }
+
+        var result = data.replace(/url\('([^/].*)'\)|url\("([^/].*)"\)/gi, "url('../../themes/" + themeName + "/$1')");
+        fs.writeFile(file, result, 'utf8', function (err) {
+           if (err) return console.log(err);
+        });
+      });
+      
+    })
   }
 }
 
--- webpack.browser.js
+++ webpack.browser.js
@@ -14,6 +14,7 @@ fs.writeFileSync(path.resolve(buildDir, 'hash.md5'), digest);
 
 module.exports = {
   entry:  './build/out/browser/index.js',
+  target: 'electron',
   output: {
     path: buildDir,
     filename: 'browser.bundle.js',
@@ -47,11 +48,14 @@ module.exports = {
       )},
       { test: /\.json$/, use: 'json-loader' },
       { test: /\.html$/, use: 'file-loader?name=[name].[ext]' },
+      { test: /\.md$/, use: 'raw-loader' },
+      { test: /\.txt$/, use: 'raw-loader' },
       { test: /\.(jpg|png|gif)$/, use: 'file-loader' },
       { test: /\.js.map$/, use: 'file-loader' },
       { test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/, use: 'url-loader?limit=10000&mimetype=application/font-woff' },
       { test: /\.woff(\?v=\d+\.\d+\.\d+)?$/, use: 'url-loader?limit=10000&mimetype=application/font-woff' },
       { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, use: 'url-loader?limit=10000&mimetype=application/octet-stream' },
+      { test: /\.otf(\?v=\d+\.\d+\.\d+)?$/, use: 'url-loader?limit=10000&mimetype=application/octet-stream' },
       { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, use: 'file-loader' },
       { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, use: 'url-loader?limit=10000&mimetype=image/svg+xml' }
     ],
@@ -61,9 +65,7 @@ module.exports = {
     child_process: 'commonjs child_process'
   },
   node: {
-    fs: 'empty',
-    __dirname: false,
-    __filename: false
+    fs: 'empty'
   },
   plugins: [
     new ExtractTextPlugin("styles.css"),
