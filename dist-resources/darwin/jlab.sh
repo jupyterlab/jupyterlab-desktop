63cc3aa4ddb7f53cabe512467af1ec802b04f4db
Mehmet Bektas <mbektasgh@outlook.com>
2021-12-12 20:52:09 -0800

    use python3 explicitly

--- dist-resources/darwin/jlab.sh
+++ dist-resources/darwin/jlab.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 # calculate application path from this script's path
-JLAB_PATH=$(python -c "import sys; from os import path; self_path=path.realpath(sys.argv[1]); print(path.normpath(path.join(self_path, '../../../MacOS/JupyterLab')));" "${BASH_SOURCE[0]}");
+JLAB_PATH=$(python3 -c "import sys; from os import path; self_path=path.realpath(sys.argv[1]); print(path.normpath(path.join(self_path, '../../../MacOS/JupyterLab')));" "${BASH_SOURCE[0]}");
 $JLAB_PATH "$@"
 exit $?
