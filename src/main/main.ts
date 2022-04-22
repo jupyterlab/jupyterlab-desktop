a04f7b1bf0adb6c5bf2c2850080052b663d2fdb7
Mehmet Bektas <mbektasgh@outlook.com>
2022-04-21 20:50:32 -0700

    skip manual check for macOS

--- src/main/main.ts
+++ src/main/main.ts
@@ -17,10 +17,11 @@ autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
     buttons: ['Restart', 'Later'],
     title: 'Application Update',
     message: process.platform === 'win32' ? releaseNotes : releaseName,
-    detail: 'A new version has been downloaded. Restart the application to apply the updates.'
+    detail:
+      'A new version has been downloaded. Restart the application to apply the updates.'
   };
 
-  dialog.showMessageBox(dialogOpts).then((returnValue) => {
+  dialog.showMessageBox(dialogOpts).then(returnValue => {
     if (returnValue.response === 0) autoUpdater.quitAndInstall();
   });
 });
