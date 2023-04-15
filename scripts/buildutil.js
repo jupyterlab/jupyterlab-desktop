beab133190b5cfa17973fb8b64c842d75172f220
Mehmet Bektas <mbektasgh@outlook.com>
2023-04-14 18:46:08 -0700

    remove set_jupyterlab_version CLI

--- scripts/buildutil.js
+++ scripts/buildutil.js
@@ -13,7 +13,6 @@ const cli = meow(
 
     Options
       --check-version-match      check for JupyterLab version match
-      --set-jupyterlab-version   set JupyterLab version
 
     Other options:
       --help                     show usage information
@@ -27,60 +26,10 @@ const cli = meow(
       checkVersionMatch: {
         type: 'boolean',
         default: false
-      },
-      setJupyterlabVersion: {
-        type: 'string',
-        default: ''
       }
     }
   }
 );
-
-// remove ~ or ^ prefix from semver version
-function makeVersionAbsolute(version) {
-  if (version.length > 0 && (version[0] === '^' || version[0] === '~')) {
-    return version.substring(1);
-  }
-
-  return version;
-}
-
-/*
- * Checks if JupyterLab extensions listed in package.json
- * are properly included into the bundle in "extensions/index.ts"
- */
-function checkExtensionImports() {
-  console.log('Checking for missing extension imports...');
-
-  const pkgjsonFileData = fs.existsSync(pkgjsonFilePath)
-    ? fs.readJSONSync(pkgjsonFilePath)
-    : undefined;
-  if (!pkgjsonFileData) {
-    console.error('package.json not found!');
-    process.exit(1);
-  }
-
-  const extensions = pkgjsonFileData.jupyterlab.extensions;
-  const mimeExtensions = pkgjsonFileData.jupyterlab.mimeExtensions;
-  const extensionsFilePath = path.resolve(
-    __dirname,
-    `../src/browser/extensions/index.ts`
-  );
-  let extensionsFileContent = '';
-  try {
-    extensionsFileContent = fs.readFileSync(extensionsFilePath, 'utf8');
-  } catch (e) {
-    console.error('Error loading "extensions/index.ts"', e);
-  }
-  for (const extension of [...extensions, ...mimeExtensions]) {
-    if (!extensionsFileContent.includes(`require('${extension}')`)) {
-      console.error(`${extension} is not imported in "extensions/index.ts"`);
-      process.exit(1);
-    }
-  }
-
-  console.log('All extensions are bundled correctly.');
-}
 
 if (cli.flags.checkVersionMatch) {
   // parse application version
@@ -138,116 +87,4 @@ if (cli.flags.checkVersionMatch) {
 
   console.log('JupyterLab version match satisfied!');
   process.exit(0);
-}
-
-if (cli.flags.setJupyterlabVersion !== '') {
-  const https = require('https');
-  const newVersion = cli.flags.setJupyterlabVersion;
-  console.log(`Downloading JupyterLab v${newVersion} package.json`);
-
-  const url = `https://raw.githubusercontent.com/jupyterlab/jupyterlab/v${newVersion}/jupyterlab/staging/package.json`;
-
-  https
-    .get(url, res => {
-      let body = '';
-
-      res.on('data', chunk => {
-        body += chunk;
-      });
-
-      res.on('end', () => {
-        try {
-          let newPkgData = JSON.parse(body);
-          if (!(newPkgData.devDependencies && newPkgData.resolutions)) {
-            console.error(`Invalid package.json format for v${newVersion}!`);
-            process.exit(1);
-          }
-
-          const newResolutions = { ...newPkgData.resolutions };
-          const newDependencies = {
-            ...newPkgData.devDependencies,
-            ...newResolutions
-          };
-
-          const pkgjsonFileData = fs.existsSync(pkgjsonFilePath)
-            ? fs.readJSONSync(pkgjsonFilePath)
-            : undefined;
-          if (!pkgjsonFileData) {
-            console.error('package.json not found!');
-            process.exit(1);
-          }
-
-          const oldDependencies = pkgjsonFileData.dependencies;
-
-          for (const packageName in oldDependencies) {
-            if (
-              packageName.startsWith('@jupyterlab') &&
-              packageName in newDependencies
-            ) {
-              oldDependencies[packageName] = makeVersionAbsolute(
-                newDependencies[packageName]
-              );
-            }
-          }
-
-          // copy resolutions and singleton packages as-is
-          pkgjsonFileData.resolutions = newResolutions;
-          pkgjsonFileData.singletonPackages = newPkgData.singletonPackages;
-
-          fs.writeFileSync(
-            pkgjsonFilePath,
-            JSON.stringify(pkgjsonFileData, null, 2)
-          );
-
-          console.log(`JupyterLab dependencies updated to v${newVersion}`);
-
-          checkExtensionImports();
-
-          // Check if all extensions of the new JupyterLab version are
-          // bundled into the application
-          console.log('Checking for extension list match...');
-          const extensions = pkgjsonFileData.jupyterlab.extensions;
-          const mimeExtensions = pkgjsonFileData.jupyterlab.mimeExtensions;
-          const excludedExtensions =
-            pkgjsonFileData.jupyterlab.excludedExtensions;
-
-          const extensionSet = new Set([...extensions, ...excludedExtensions]);
-          const mimeExtensionSet = new Set([
-            ...mimeExtensions,
-            ...excludedExtensions
-          ]);
-
-          for (const extension in newPkgData.jupyterlab.extensions) {
-            if (!extensionSet.has(extension)) {
-              console.error(
-                `JupyterLab v${newVersion} ${extension} is not bundled into the application!`
-              );
-              process.exit(1);
-            }
-          }
-
-          for (const extension in newPkgData.jupyterlab.mimeExtensions) {
-            if (!mimeExtensionSet.has(extension)) {
-              console.error(
-                `JupyterLab v${newVersion} ${extension} is not bundled into the application!`
-              );
-              process.exit(1);
-            }
-          }
-
-          console.log(
-            'All extensions in the new version are bundled correctly.'
-          );
-
-          process.exit(0);
-        } catch (error) {
-          console.error(error.message);
-          process.exit(1);
-        }
-      });
-    })
-    .on('error', error => {
-      console.error(error.message);
-      process.exit(1);
-    });
 }
