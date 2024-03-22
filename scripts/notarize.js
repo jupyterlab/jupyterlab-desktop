9051ab9d7d0ea1b458e34513eddcc5b1f2599889
Mehmet Bektas <mbektasgh@outlook.com>
2024-03-22 12:07:53 -0700

    Update notarize.js

--- scripts/notarize.js
+++ scripts/notarize.js
@@ -18,6 +18,6 @@ exports.default = async function notarizing(context) {
     appPath: `${appOutDir}/${appName}.app`,
     appleId: process.env.APPLEID,
     appleIdPassword: process.env.APPLEIDPASS,
-    tool: "notarytool"
+    tool: 'notarytool'
   });
 };
